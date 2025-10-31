97047bebb482e5d4d584fe2a86af82e4a50f6c4e
aaranya1 <aniketaranya1@gmail.com>
2025-10-30 21:11:12 -0400

    task 10 and 11 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,9 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
+
+const BASE_URL = 'https://aniketaranya-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
 
 
 public_users.post("/register", (req,res) => {
@@ -55,5 +58,26 @@ public_users.get('/review/:isbn',function (req, res) {
   let isbn = req.params.isbn;
   res.send(books[isbn].reviews);
 });
+
+// Promise callback for getting list of books (Task 10)
+axios.get(`${BASE_URL}/`)
+  .then(response => {
+    console.log("List of books available in the shop:");
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error("Error fetching book list:", error.message);
+  });
+
+async function getBookByISBN(isbn){
+    try{
+        const response = await axios.get(`${BASE_URL}/isbn/${isbn}`);
+        console.log(`Book details for ISBN ${isbn}: `);
+        console.log(response.data);
+    }
+    catch(error){
+        console.error(`Error fetching book with ISBN ${isbn}:`, error.message);
+    }
+}
 
 module.exports.general = public_users;
